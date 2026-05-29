f2e81bdd0adbfacbef0985ba88afd79f0b6b483c
WigoSas <rodrisalas17@gmail.com>
2026-05-29 08:40:24 +0200

    docs: add post module documentation

--- Core/Inc/pedals/post-api.h
+++ Core/Inc/pedals/post-api.h
@@ -3,6 +3,12 @@
 
 #include "post.h"
 
+/*!
+ * \brief Initialisation of other module's external functions
+ * 
+ * \param init structure containing all the external function
+ * \return enum PostReturnCode operation status, fails if init is NULL or subroutines fail
+ */
 enum PostReturnCode post_init(struct PostInit* init);
 
 #endif /* POST_API_H */
--- Core/Inc/pedals/post.h
+++ Core/Inc/pedals/post.h
@@ -4,15 +4,24 @@
 #include "voltage-scaling.h"
 #include "throttle.h"
 
+
+/*!
+ * \brief Return code for this module's operations
+ * 
+ */
 enum PostReturnCode{
-	POST_RC_OK,
-	POST_RC_ERROR
+	POST_RC_OK,		/*!< Operation success*/
+	POST_RC_ERROR	/*!< Either the initialisation subroutines failed or some field was NULL*/
 };
 
+/*!
+ * \brief Structure with pointers to external functions, used for initialisation
+ * 
+ */
 struct PostInit {
-	voltage_scaling_sensor_read_callback read_raw_values;
-	throttle_timer_callback start_timer;
-	throttle_timer_callback stop_timer;
+	voltage_scaling_sensor_read_callback read_raw_values;	/*!< External function to read sensors' raw values*/
+	throttle_timer_callback start_timer;					/*!< External function to start implausibility timer*/
+	throttle_timer_callback stop_timer;						/*!< External function to stop implausibility timer*/
 };
 
 #endif /* POST_H */
