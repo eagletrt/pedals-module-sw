e2bfa6fa7c69e09059a11b3ee6fbeaeb5361c7f7
WigoSas <rodrisalas17@gmail.com>
2026-06-05 23:19:32 +0200

    feat: bring bots module files from old branch

---
+++ Core/Src/pedals/bots-api.c
@@ -0,0 +1,17 @@
+#include "bots-api.h"
+#include "eagletrt-api.h"
+
+EAGLETRT_STATIC bool bots_is_trigger_activated = false;
+
+void bots_set_triggered(void) {
+    bots_is_trigger_activated = true;
+}
+
+bool bots_is_triggered(void) {
+    if (bots_is_trigger_activated) {
+        bots_is_trigger_activated = false;
+        return true;
+    }
+
+    return false;
+}
