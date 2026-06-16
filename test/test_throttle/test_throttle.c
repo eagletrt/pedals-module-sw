74262b3c1a6da25425c876d7e2f8d0a6d25a8386
WigoSas <rodrisalas17@gmail.com>
2026-06-16 23:35:14 +0200

    test: add some of the tests for update function

--- test/test_throttle/test_throttle.c
+++ test/test_throttle/test_throttle.c
@@ -1,23 +1,20 @@
 #include "unity.h"
 #include "fff.h"
 #include "throttle-api.h"
-#include "voltage-scaling-api.h"
-#include "stdint.h"
 
 DEFINE_FFF_GLOBALS;
 
-FAKE_VALUE_FUNC(int32_t, VOLTAGE_SCALING_read_raw, enum SensorTypesName);
 FAKE_VALUE_FUNC(enum ThrottleReturnCode, THROTTLE_start_timer);
 FAKE_VALUE_FUNC(enum ThrottleReturnCode, THROTTLE_reset_timer);
 
-extern struct VoltageScalingHandler voltage_scaling_handler;
 extern struct ThrottleHandler throttle_handler;
 
 
 void setUp(void) {
-    RESET_FAKE(VOLTAGE_SCALING_read_raw);
+    RESET_FAKE(THROTTLE_start_timer);
+	RESET_FAKE(THROTTLE_reset_timer)
     FFF_RESET_HISTORY();
-	voltage_scaling_handler.read_raw = NULL;
+	throttle_handler.is_implausibility_timeout = false;
 	throttle_handler.error_status = THROTTLE_RC_NO_ERROR;
 	throttle_handler.last_throttle_value = 0.0f;
 	throttle_handler.start_timer = NULL;
@@ -29,6 +26,7 @@ void setUp(void) {
     // clean stuff up here
 }*/
 
+// throttle_implausibility_timeout_trigger will not be tested because it's a simple setter
 
 void test_throttle_successful_init(void){
 	enum ThrottleReturnCode rc = throttle_init(THROTTLE_start_timer,THROTTLE_reset_timer);
@@ -55,33 +53,85 @@ void test_throttle_init_failure(void){
 	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.stop_timer,NULL,"Reset timer was not initialised");
 }
 
-void test_throttle_timer_trigger(){
-	throttle_timer_trigger();
-	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.error_status,THROTTLE_RC_IMPLAUSIBILITY,"Error was not about implausibility");
-}
+// check if the implausible error state and related actions are as intended
+void test_throttle_update_to_implausible_error(){
+	enum ThrottleReturnCode rc = throttle_init(THROTTLE_start_timer,THROTTLE_reset_timer);
 
-void test_throttle_get_error_from_ok(){
 	throttle_handler.error_status = THROTTLE_RC_NO_ERROR;
-	TEST_ASSERT_EQUAL_MESSAGE(throttle_get_error_status(),THROTTLE_RC_NO_ERROR,"An error status was found");
-	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.error_status, THROTTLE_RC_NO_ERROR, "An error status remained in the handler");
-}
-
-void test_throttle_get_error_from_callback_failure(){
-	throttle_handler.error_status = THROTTLE_RC_CALLBACK_FAILURE;
-	TEST_ASSERT_EQUAL_MESSAGE(throttle_get_error_status(),THROTTLE_RC_CALLBACK_FAILURE,"Error status was not about callback");
-	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.error_status, THROTTLE_RC_NO_ERROR, "An error status remained in the handler");
-}
-
-void test_throttle_get_error_from_implausibility(){
-	throttle_handler.error_status = THROTTLE_RC_IMPLAUSIBILITY;
 	throttle_handler.throttle_status = THROTTLE_STATUS_OK;
 	throttle_handler.last_throttle_value = 0.5f;
-	TEST_ASSERT_EQUAL_MESSAGE(throttle_get_error_status(),THROTTLE_RC_IMPLAUSIBILITY,"An error status was found");
-	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.error_status, THROTTLE_RC_NO_ERROR, "An error status remained in the handler");
+
+	throttle_implausibility_timeout_trigger();
+
+	throttle_update_pedal_values(0.3F, 0.32F, 0.31F); // actual values don't matter, will be reset to zero
+
+	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.error_status, THROTTLE_RC_IMPLAUSIBILITY, "Error was not implausibility");
 	TEST_ASSERT_EQUAL_FLOAT_MESSAGE(throttle_handler.last_throttle_value, 0.0f, "Last value was not reset");
 	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.throttle_status, THROTTLE_STATUS_IMPLAUSIBLE_ERROR, "Status was not set to implausibility");
 }
 
+void test_throttle_update_stay_in_implausible_error(){
+	enum ThrottleReturnCode rc = throttle_init(THROTTLE_start_timer,THROTTLE_reset_timer);
+
+	throttle_handler.error_status = THROTTLE_RC_NO_ERROR;
+	throttle_handler.throttle_status = THROTTLE_STATUS_IMPLAUSIBLE_ERROR;
+	throttle_handler.last_throttle_value = 0.0f;
+
+	throttle_update_pedal_values(0.3F, 0.32F, 0.31F); // actual values don't matter, will be reset to zero
+
+	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.error_status, THROTTLE_RC_NO_ERROR, "An error was found");
+	TEST_ASSERT_EQUAL_FLOAT_MESSAGE(throttle_handler.last_throttle_value, 0.0f, "Last value was changed, even if it shouldn't have");
+	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.throttle_status, THROTTLE_STATUS_IMPLAUSIBLE_ERROR, "Status was changed from implausibility");
+}
+
+// check if these values are handled correctly and keep the throttle in NO_ERROR state
+
+void test_throttle_update_all_values_valid(){
+	float apps1 = 0.50F;
+	float apps2 = 0.52F;
+	float apps3 = 0.54F;
+
+	enum ThrottleReturnCode rc = throttle_init(THROTTLE_start_timer,THROTTLE_reset_timer);
+	throttle_update_pedal_values(apps1,apps2,apps3);
+
+	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.error_status, THROTTLE_RC_NO_ERROR, "An error was found");
+	TEST_ASSERT_EQUAL_FLOAT_MESSAGE(throttle_handler.last_throttle_value, 0.52f, "Value wasn't as expected");
+	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.throttle_status, THROTTLE_STATUS_OK, "Status was changed from NO_ERROR");
+}
+
+void test_throttle_update_two_values_valid_one_out_of_range(){
+	float apps1 = 0.93F;
+	float apps2 = 0.97F;
+	float apps3 = 1.01F; // out of range
+
+	enum ThrottleReturnCode rc = throttle_init(THROTTLE_start_timer,THROTTLE_reset_timer);
+	throttle_update_pedal_values(apps1,apps2,apps3);
+
+	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.error_status, THROTTLE_RC_NO_ERROR, "An error was found");
+	TEST_ASSERT_EQUAL_FLOAT_MESSAGE(throttle_handler.last_throttle_value, 0.95f, "Value wasn't as expected");
+	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.throttle_status, THROTTLE_STATUS_OK, "Status was changed from NO_ERROR");
+}
+//--------------------------------------------------
+//--------------------------------------------------
+// check OK -> IMPLAUSIBLE_RECOVERABLE behaviours
+void test_throttle_update_to_implausible(){
+	float apps1 = 0.1F;
+	float apps2 = 0.25F;
+	float apps3 = 0.40F;
+
+	throttle_handler.last_throttle_value = 0.4F;
+
+	THROTTLE_start_timer_fake.return_val = THROTTLE_RC_NO_ERROR;
+
+	enum ThrottleReturnCode rc = throttle_init(THROTTLE_start_timer,THROTTLE_reset_timer);
+	throttle_update_pedal_values(apps1,apps2,apps3);
+
+	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.error_status, THROTTLE_RC_NO_ERROR, "An error was found");
+	TEST_ASSERT_EQUAL_FLOAT_MESSAGE(throttle_handler.last_throttle_value, 0.40f, "Value wasn't as expected");
+	TEST_ASSERT_EQUAL_MESSAGE(throttle_handler.throttle_status, THROTTLE_STATUS_IMPLAUSIBLE_RECOVERABLE, "Status was changed from NO_ERROR");
+	TEST_ASSERT_EQUAL_MESSAGE(1, THROTTLE_start_timer_fake.call_count, "Start timer wasn't called exactly once");
+}
+/*
 void test_throttle_internal_percentage_callback_failure(void){
 	int32_t apps1 = THROTTLE_APPS_1_MIN_VALUE + (THROTTLE_APPS_1_MAX_VALUE - THROTTLE_APPS_1_MIN_VALUE) / 2; //50%
 	int32_t apps2 = THROTTLE_APPS_2_MIN_VALUE + (THROTTLE_APPS_2_MAX_VALUE - THROTTLE_APPS_2_MIN_VALUE) / 2; //50%
@@ -96,6 +146,7 @@ void test_throttle_internal_percentage_callback_failure(void){
 	TEST_ASSERT_EQUAL_FLOAT_MESSAGE(res,0.0f,"Result was not equal to zero");
 	TEST_ASSERT_EQUAL_MESSAGE(throttle_get_error_status(),THROTTLE_RC_CALLBACK_FAILURE,"Return was not about callback failure");
 }
+
 /*
 
 void test_throttle_three_ok_sensors_valid(void) { 
@@ -223,19 +274,50 @@ void test_throttle_is_timer_reset(void) {
 }
 */
 
+void test_throttle_get_travel_percentage_no_error(void){
+	enum ThrottleReturnCode rc = throttle_init(THROTTLE_start_timer,THROTTLE_reset_timer);
+
+	throttle_handler.last_throttle_value = 0.4F;
+	throttle_handler.error_status = THROTTLE_RC_NO_ERROR;
+
+	struct ThrottleReturnValue ret = throttle_get_travel_percentage();
+	TEST_ASSERT_EQUAL_FLOAT_MESSAGE(0.4F,ret.throttle_value,"Return percentage was not as expected");
+	TEST_ASSERT_EQUAL_MESSAGE(THROTTLE_RC_NO_ERROR,ret.throttle_error,"An error was returned");
+}
+
+void test_throttle_get_travel_percentage_error_found(void){
+	enum ThrottleReturnCode rc = throttle_init(THROTTLE_start_timer,THROTTLE_reset_timer);
+
+	throttle_handler.last_throttle_value = 0.5F;
+	throttle_handler.error_status = THROTTLE_RC_IMPLAUSIBILITY;
+
+	struct ThrottleReturnValue ret = throttle_get_travel_percentage();
+	TEST_ASSERT_EQUAL_MESSAGE(0.5F,ret.throttle_value,"Return percentage was not as expected");
+	TEST_ASSERT_EQUAL_MESSAGE(THROTTLE_RC_IMPLAUSIBILITY,ret.throttle_error,"Error should have been implausibility");
+	TEST_ASSERT_EQUAL_MESSAGE(THROTTLE_RC_NO_ERROR,throttle_handler.error_status,"Internal error status did not go back to NO_ERROR");
+}
+
 int main( int argc, char **argv) {
     UNITY_BEGIN();
 	
 	RUN_TEST(test_throttle_successful_init);
 	RUN_TEST(test_throttle_init_failure);
 
-	RUN_TEST(test_throttle_timer_trigger);
+	RUN_TEST(test_throttle_update_stay_in_implausible_error);
+	RUN_TEST(test_throttle_update_to_implausible_error);
 
-	RUN_TEST(test_throttle_get_error_from_ok);
-	RUN_TEST(test_throttle_get_error_from_callback_failure);
-	RUN_TEST(test_throttle_get_error_from_implausibility);
+	RUN_TEST(test_throttle_update_all_values_valid);
+	RUN_TEST(test_throttle_update_two_values_valid_one_out_of_range);
 
-	RUN_TEST(test_throttle_internal_percentage_callback_failure);
+	RUN_TEST(test_throttle_update_to_implausible);
+
+	//RUN_TEST(test_throttle_timer_trigger);
+
+	//RUN_TEST(test_throttle_get_error_from_ok);
+	//RUN_TEST(test_throttle_get_error_from_callback_failure);
+	//RUN_TEST(test_throttle_get_error_from_implausibility);
+
+	//RUN_TEST(test_throttle_internal_percentage_callback_failure);
 
 	//RUN_TEST(test_throttle_get_error_status_false);
 	//RUN_TEST(test_throttle_get_error_status_true);
@@ -248,5 +330,8 @@ int main( int argc, char **argv) {
 
 	//RUN_TEST(test_throttle_is_timer_start);
 	//RUN_TEST(test_throttle_is_timer_reset);
+
+	RUN_TEST(test_throttle_get_travel_percentage_no_error);
+	RUN_TEST(test_throttle_get_travel_percentage_error_found);
     UNITY_END();
 }
